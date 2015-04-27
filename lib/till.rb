b97656f0649468d9c069aedf5a239de99232374b
Edward Brown <edward.brown.london@gmail.com>
2015-04-27 13:50:56 +0100

    added tax and totals

--- lib/till.rb
+++ lib/till.rb
@@ -1,20 +1,40 @@
 class Till
 
   attr_accessor :items
+  attr_accessor :price
   attr_accessor :quantity
+  attr_reader :line_total
+  attr_reader :pre_tax_total
+  attr_reader :tax
+  attr_reader :tax_rate
+  attr_reader :total
 
   def initialize
+    @tax_rate = 1.0864
     @items = Array.new
     @quantity = Array.new
+    @price = Array.new
+    @line_total = Array.new
+    path = File.expand_path('../../hipstercoffee.json', __FILE__)
+    @menu = JSON.parse(File.read(path))
   end
 
   def add_entry item, quantity
     @items.push item
     @quantity.push quantity
+    price = @menu[0]['prices'][0][item]
+    @price.push price
+    @line_total.push price * quantity
   end
 
   def reset!
     initialize
+  end
+
+  def checkout
+    @pre_tax_total = line_total.inject(:+)
+    @tax = @tax_rate * @pre_tax_total
+    @total = @pre_tax_total + @tax
   end
 
 end
--- spec/till_spec.rb
+++ spec/till_spec.rb
@@ -15,8 +15,38 @@ describe Till do
     till.reset!
     expect(till.items).to eq([])
     expect(till.quantity).to eq([])
+    expect(till.price).to eq([])
   end
 
+  it 'should add the price of the ordered item' do
+    till.add_entry('Cappucino', 2)
+    expect(till.price).to eq([3.85])
+  end
+
+  it 'should calculate the line total amount' do
+    till.add_entry('Cappucino', 2)
+    expect(till.line_total).to eq([7.70])
+  end
+
+  it 'should calculate the total amount' do
+    till.add_entry('Cappucino', 2)
+    till.add_entry('Choc Mudcake', 2)
+    till.checkout
+    expect(till.pre_tax_total).to eq(20.50)
+  end
+
+  it 'should be able to work out the tax' do
+    till.add_entry('Cappucino', 2)
+    till.add_entry('Choc Mudcake', 1)
+    till.checkout
+    expect(till.tax).to eq(till.pre_tax_total * till.tax_rate)
+  end
+
+  it 'should be able to work out the total' do
+    till.add_entry('Cappucino', 2)
+    till.checkout
+    expect(till.total).to eq(till.pre_tax_total + till.tax)
+  end
 
 
 end
